b526bac822ed72feebc7a6e577b1dd68f18e1d3f
Vanja Zeli <vanja.zeli@vegait.rs>
2023-01-06 16:40:20 +0100

    feat: add unfold effect

--- app/main.js
+++ app/main.js
@@ -2,6 +2,8 @@ import "../scss/style.scss";
 
 import hover from "./hover";
 import fadeIra from "./animations/fade";
+import unfoldIra from "./animations/unfold";
 
 hover.init();
 fadeIra.init();
+unfoldIra.init();
