08c65fa989966470aaf9ca3da0133e6a069f426e
vanjazeli <vanjazeli@gmail.com>
2023-01-11 13:03:43 +0100

    feat: add slide animation

--- app/main.js
+++ app/main.js
@@ -212,3 +212,146 @@ const unfoldBackgorunds = [
 unfoldElements.forEach((unfoldElement, unfoldElementIndex) => {
   new IraUnfold(unfoldElement, unfoldBackgorunds[unfoldElementIndex]);
 });
+
+// Slide animation
+
+class IraSlide {
+  constructor(element, imageUrl) {
+    this.main = { element: element };
+    this.main.reveal = document.createElement("div");
+    this.main.reveal.className = "reveal";
+    this.main.reveal.innerHTML = `<div class="reveal__inner"><div class="reveal__image" style="background: url('${imageUrl}') no-repeat center center / cover"></div></div>`;
+    this.main.reveal.style.overflow = "hidden";
+    this.main.element.appendChild(this.main.reveal);
+    this.main.inner = this.main.reveal.querySelector(".reveal__inner");
+    this.main.inner.style.overflow = "hidden";
+    this.main.image = this.main.reveal.querySelector(".reveal__image");
+    this.init();
+  }
+  init() {
+    this.events();
+  }
+  mousePosition(e) {
+    let posx = e.pageX;
+    let posy = e.pageY;
+    if (!e) var e = window.event;
+    if (e.pageX || e.pageY) {
+      posx = e.pageX;
+      posy = e.pageY;
+    } else if (e.clientX || e.clientY) {
+      posx =
+        e.clientX +
+        document.body.scrollLeft +
+        document.documentElement.scrollLeft;
+      posy =
+        e.clientY +
+        document.body.scrollTop +
+        document.documentElement.scrollTop;
+    }
+    return { x: posx, y: posy };
+  }
+  events() {
+    this.positionElement = (e) => {
+      const mousePosition = this.mousePosition(e);
+      this.main.reveal.style.top = mousePosition.y + 30 + "px";
+      this.main.reveal.style.left = mousePosition.x + 30 + "px";
+    };
+    this.mouseEnter = (e) => {
+      this.positionElement(e);
+      this.showImage();
+    };
+    this.mouseMove = (e) =>
+      requestAnimationFrame(() => {
+        this.positionElement(e);
+      });
+    this.mouseLeave = () => {
+      this.hideImage();
+    };
+    this.main.element.addEventListener("mouseenter", this.mouseEnter);
+    this.main.element.addEventListener("mousemove", this.mouseMove);
+    this.main.element.addEventListener("mouseleave", this.mouseLeave);
+  }
+  showImage() {
+    gsap.killTweensOf(this.main.reveal);
+    gsap.killTweensOf(this.main.image);
+    this.tl = gsap
+      .timeline({
+        onStart: () => {
+          this.main.reveal.style.display = "block";
+        },
+      })
+      .add("start")
+      .add(
+        gsap.fromTo(
+          this.main.inner,
+          { x: -50, y: 200, rotate: 50 },
+          { x: 0, y: 0, rotate: 0, duration: 0.3, ease: "power2.out" }
+        ),
+        "start"
+      )
+      .add(
+        gsap.fromTo(
+          this.main.image,
+          { x: 50, y: -200, rotate: -50 },
+          { x: 0, y: 0, rotate: 0, duration: 0.3, ease: "power2.out" }
+        ),
+        "start"
+      )
+      .add(
+        gsap.fromTo(
+          this.main.image,
+          { scale: 1.8 },
+          { scale: 1, duration: 0.5 }
+        ),
+        "start"
+      );
+  }
+  hideImage() {
+    gsap.killTweensOf(this.main.reveal);
+    gsap.killTweensOf(this.main.image);
+    this.tl = gsap
+      .timeline({
+        onComplete: () => {
+          this.main.reveal.style.display = "none";
+          gsap.set(this.main.inner, { x: "", y: "", rotate: "" });
+          gsap.set(this.main.image, { x: "", y: "", rotate: "", scale: "" });
+        },
+      })
+      .add("end")
+      .add(
+        gsap.fromTo(
+          this.main.inner,
+          { x: 0, y: 0, rotate: 0 },
+          { x: -80, y: -200, rotate: -30, duration: 0.3, ease: "power2.out" }
+        ),
+        "end"
+      )
+      .add(
+        gsap.fromTo(
+          this.main.image,
+          { x: 0, y: 0, rotate: 0, scale: 1 },
+          {
+            x: 80,
+            y: 200,
+            rotate: 30,
+            duration: 0.3,
+            scale: 2,
+            ease: "power2.out",
+          }
+        ),
+        "end"
+      );
+  }
+}
+
+const slideElements = document.querySelectorAll(".js-ira-slide");
+const slideBackgrounds = [
+  "./images/3-1.png",
+  "./images/3-2.png",
+  "./images/3-3.png",
+  "./images/3-4.png",
+];
+
+slideElements.forEach((slideElement, slideElementIndex) => {
+  new IraSlide(slideElement, slideBackgrounds[slideElementIndex]);
+});
